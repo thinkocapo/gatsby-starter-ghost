9b17336d7cf3d9bb9cbce1d2853e10c972a70037
William Capozzoli <thinkocapo@gmail.com>
2024-04-26 22:54:27 +0200

    defined siteUrl elsewhere

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -2,7 +2,8 @@ const config = require(`../../.ghost.json`).production;
 module.exports = {
     siteUrl:
         process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
+            // ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
+            ? process.env.SITE_URL || "coruscating-otter-1c1142" || `http://localhost:8000`
             : `http://localhost:8000`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
